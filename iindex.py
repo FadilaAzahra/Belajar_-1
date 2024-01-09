29c7d7587e8659ddc969caa63ca6133fd4433031
FadilaAzahra <zhraadilaa@gmail.com>
2024-01-09 08:55:54 +0700

    python

---
+++ iindex.py
@@ -0,0 +1,32 @@
+# 1 Menebak Apakah Kamu Balita?
+umur= float(input("Masukkan angka umur: "))
+
+if umur <= 5:
+    print("Selamat Anda Balita.")
+else:
+    print("Anda Sudah Melewati Masa Balita.")
+
+# 2 Menebak Apakah Kamu Anak-anak?
+umur= float(input("Masukkan angka umur: "))
+
+if umur > 5:
+    print("Selamat Anda Anak mamah.")
+else:
+    print("Anda Sudah Melewati Masa Anak mamah.")
+
+# 3 Menebak Apakah Kamu Remaja?
+umur= float(input("Masukkan angka umur: "))
+
+if umur > 12:
+    print("Selamat Anda Remaja.")
+else:
+    print("Anda Sudah Melewati Masa Remaja.")
+
+# 4 Menebak Apakah Kamu Dewasa?
+umur= float(input("Masukkan angka umur: "))
+
+if umur > 18:
+    print("Selamat Anda Menjadi Dewasa.")
+else:
+    print("Anda Harus Perbanyak Amalan.")
+
